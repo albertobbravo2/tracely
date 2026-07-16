9ffb3eb43e1fe0f2a44aa7977b387bcb323a9ad8
jaimeele <jaimecamposlucena@gmail.com>
2026-07-16 21:51:15 +0200

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+
+<x-layouts::app :title="__('Home')">
+ <h1 class="flex justify-center font-bold text-2xl"> Tracely </h1>
+
+
+</x-layouts::app>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.{blade.php,js}",
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./vendor/livewire/flux/stubs/**/*.blade.php",
+    "./vendor/livewire/flux-pro/stubs/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
