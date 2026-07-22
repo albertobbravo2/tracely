b97ea015894d5d181441a022103f2144c97a24d3
jaimeele <jaimecamposlucena@gmail.com>
2026-07-22 18:41:12 +0200

    colors

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,11 @@
-
 <x-layouts::app :title="__('Home')">
- <h1 class="flex justify-center font-bold text-2xl"> Tracely </h1>
+    <div class="mx-auto flex max-w-md flex-col items-center gap-6 py-12 text-center">
+        <flux:heading size="xl">Tracely</flux:heading>
 
-
+        <flux:input placeholder="Buscar..." class="w-full">
+            <x-slot name="iconTrailing">
+                <flux:button size="sm" variant="subtle" icon="magnifying-glass" class="-mr-1" />
+            </x-slot>
+        </flux:input>
+    </div>
 </x-layouts::app>
