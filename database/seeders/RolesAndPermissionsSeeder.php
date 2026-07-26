13de8f2e20531081771d836d315cd95bf6e8ed81
jaimeele <jaimecamposlucena@gmail.com>
2026-07-26 23:42:36 +0200

    permissions

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -19,17 +19,45 @@ class RolesAndPermissionsSeeder extends Seeder
         //Eliminar caché actual de permisos y roles
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        //Permisos
-        Permission::create(["name" => "gestionar estado pedido"]);
 
-        //
+        //ROLES
         $cliente = Role::create(["name" => "cliente"]);
         $agente = Role::create(["name" => "agente"]);
-        $admin = Role::create(["name" => "admin"]);
+        $administrador = Role::create(["name" => "administrador"]);
+
+        $empleado = [$agente, $administrador];
+        $todos = [$cliente, $agente, $administrador];
+        //PERMISOS
+
+        //shipment
+        Permission::create(["name" => "crear pedido"])->syncRoles($empleado);
+        Permission::create(["name" => "ver pedido"])->syncRoles($todos);
+        Permission::create(["name" => "editar pedido"])->syncRoles($empleado);
+        Permission::create(["name" => "eliminar pedido"])->syncRoles($empleado);
+        //shipment_history
+        Permission::create(["name" => "crear historial de pedido"])->syncRoles($empleado);
+        Permission::create(["name" => "ver historial de pedido"])->syncRoles($todos);
+        //user
+        Permission::create(["name" => "crear usuario"])->syncRoles($empleado);
+        Permission::create(["name" => "ver usuario"])->syncRoles($todos);
+        Permission::create(["name" => "editar usuario"])->syncRoles($empleado);
+        Permission::create(["name" => "eliminar usuario"])->syncRoles($empleado);
+        //role
+        Permission::create(["name" => "crear rol"])->syncRoles($empleado);
+        Permission::create(["name" => "ver rol"])->syncRoles($empleado);
+        Permission::create(["name" => "editar rol"])->syncRoles($empleado);
+        Permission::create(["name" => "eliminar rol"])->syncRoles($empleado);
+        //document
+        Permission::create(["name" => "crear documento"])->syncRoles($empleado);
+        Permission::create(["name" => "ver documento"])->syncRoles($todos);
+        Permission::create(["name" => "editar documento"])->syncRoles($empleado);
+        Permission::create(["name" => "eliminar documento"])->syncRoles($empleado);
+        //dashboard
+        Permission::create(["name" => "ver dashboard"])->syncRoles($cliente);
 
         //Asignar permisos a roles
-        $agente->givePermissionTo("gestionar estado pedido");
-        $admin->givePermissionTo(Permission::all());
+        $agente->givePermissionTo("crear pedido");
+        $administrador->givePermissionTo(Permission::all());
 
     }
 }
