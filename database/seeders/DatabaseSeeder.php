5a4d103d50afdbfdcf6e3b640f72570531a6f99a
jaimeele <jaimecamposlucena@gmail.com>
2026-07-29 19:03:25 +0200

    seeder with 3 users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,33 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        $this->call(RolesAndPermissionsSeeder::class);
+
+        User::create([
+            "name" => "Admin",
+            "email" => "admin@tracely.com",
+            "password" => "admin",
+            "email_verified_at" => now()
+        ])->assignRole('administrador');
+
+        User::create([
+            "name" => "Agente",
+            "email" => "agente@tracely.com",
+            "password" => "agente",
+            "email_verified_at" => now()
+        ])->assignRole('agente');
+
+        User::create([
+            "name" => "cliente",
+            "email" => "cliente@tracely.com",
+            "password" => "cliente",
+            "email_verified_at" => now()
+        ])->assignRole('cliente');
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
-    }
+            ]);
+            */
+        }
 }
